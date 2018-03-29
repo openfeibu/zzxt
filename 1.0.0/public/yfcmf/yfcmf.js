99dde438e566b0d5ec64f46963035ea74dabf3b4
wzj <wzj@119f870a-ba6c-ce45-80f7-b35f465ee3b4>
2018-03-29 09:50:44 +0000

    git-svn-id: https://39.108.144.9:8443/svn/zzxt@96 119f870a-ba6c-ce45-80f7-b35f465ee3b4

--- 1.0.0/public/yfcmf/yfcmf.js
+++ 1.0.0/public/yfcmf/yfcmf.js
@@ -1359,7 +1359,7 @@ $('body').on('click','#failSubmit',function () {
 		type:'POST',
 		data:{'n_ids':n_ids,'opinions':opinions,'change_fractions':change_fractions},
 		success: function(data){
-			if (data.code == 200) {
+			if (data.code == 1) {
                 layer.msg(data.msg,{icon: 6});
 				window.reload();
             }else{
